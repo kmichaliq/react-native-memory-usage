ccbaf9cd6c7161959c4dafe0b9998f6f61f83b69
k.michalik <k.michalik@tmtechnologie.pl>
2025-02-04 21:05:49 +0100

    Add uuid into random util

--- src/shared/utils/random.ts
+++ src/shared/utils/random.ts
@@ -3,7 +3,16 @@ const getRandomInt = (min: number, max: number) =>
 
 const getRandomBoolean = () => Math.random() < 0.5;
 
+const generateUUID = () => {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
+    const r = (Math.random() * 16) | 0;
+    const v = c === "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+};
+
 export const random = {
   int: getRandomInt,
   boolean: getRandomBoolean,
+  uuid: generateUUID,
 };
