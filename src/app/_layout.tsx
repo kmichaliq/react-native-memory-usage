33db77909e0bd0b139e3020242ae4834a2b15ba8
k.michalik <k.michalik@tmtechnologie.pl>
2025-02-04 19:39:28 +0100

    Add useMigration hook in root layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -10,13 +10,19 @@ import "@/global.css";
 import { ActivityIndicator, View } from "react-native";
 import { useDrizzleStudio } from "expo-drizzle-studio-plugin";
 import { getDbInstance } from "@/src/shared/lib/drizzle";
+import { useMigrations } from "drizzle-orm/expo-sqlite/migrator";
+import migrations from "@/src/shared/lib/drizzle/migrations/migrations";
+import { SQLiteDatabase } from "expo-sqlite";
+import { Typography } from "@/src/shared/components/Typography";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
-const DevTools = () => {
-  const { expoDb } = getDbInstance();
+type DevToolsProps = {
+  expoDb: SQLiteDatabase;
+};
 
+const DevTools = ({ expoDb }: DevToolsProps) => {
   useEffect(() => {
     console.log(
       'Drizzle studio enabled! Trigger by "shift + m" in console and then select expo-drizzle-studio-plugin'
@@ -32,6 +38,10 @@ export default function RootLayout() {
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
+  const { expoDb, db } = getDbInstance();
+
+  const { success, error } = useMigrations(db, migrations);
+
   useEffect(() => {
     if (loaded) {
       SplashScreen.hideAsync();
@@ -42,9 +52,24 @@ export default function RootLayout() {
     return null;
   }
 
+  if (error) {
+    return (
+      <View>
+        <Typography>Migration error: {error.message}</Typography>
+      </View>
+    );
+  }
+  if (!success) {
+    return (
+      <View>
+        <Typography>Migration is in progress...</Typography>
+      </View>
+    );
+  }
+
   return (
     <Suspense fallback={<ActivityIndicator size="large" />}>
-      {__DEV__ && <DevTools />}
+      {__DEV__ && <DevTools expoDb={expoDb} />}
       <SafeAreaView className="flex-1">
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
